97efaf7b0634212de16a9a8c3cbcc70fce913016
Chiku1022 <charitraagarwal1022@gmail>
2022-03-23 01:16:12 +0530

    renamed reset to change

--- app/urls.py
+++ app/urls.py
@@ -11,13 +11,15 @@ urlpatterns = [
     path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
     path('mobile/', views.mobile, name='mobile'),
+
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
-    path('resetpasswordsuccess/', auth_views.PasswordChangeDoneView.as_view(template_name='app/resetpasswordsuccess.html'), name='resetpasswordsuccess'),
+    path('changepasswordsuccess/', auth_views.PasswordChangeDoneView.as_view(template_name='app/changepasswordsuccess.html'), name='changepasswordsuccess'),
 
-    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/resetpassword.html', form_class=UserPasswordChangeForm, success_url='/resetpasswordsuccess/'), name='changepassword'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=UserPasswordChangeForm, success_url='/changepasswordsuccess/'), name='changepassword'),
     
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
