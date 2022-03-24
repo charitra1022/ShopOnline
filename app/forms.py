cb3b4928004762ddf5f5ffda984449b7320553ab
Chiku1022 <charitraagarwal1022@gmail>
2022-03-24 18:31:12 +0530

    removed autofocus

--- app/forms.py
+++ app/forms.py
@@ -22,15 +22,15 @@ class CustomerRegistrationForm(UserCreationForm):
         }
 
 class LoginForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True, 'class':'form-input', 'placeholder':'Username'}))
+    username = UsernameField(widget=forms.TextInput(attrs={'class':'form-input', 'placeholder':'Username'}))
     
     password = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password', 'class':'form-input', 'placeholder':'Password'}))
 
 class UserPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(label=_("Old password"), strip=False, widget=forms.PasswordInput(attrs={"autocomplete": "current-password", "autofocus": True, 'class':'form-control'}))
+    old_password = forms.CharField(label=_("Old password"), strip=False, widget=forms.PasswordInput(attrs={"autocomplete": "current-password", 'class':'form-input', 'placeholder':'Current Password'}))
 
-    new_password1 = forms.CharField(label=_("New password"), strip=False, widget=forms.PasswordInput(attrs={"autocomplete": "new-password", 'class':'form-control'}), help_text=password_validation.password_validators_help_text_html())
+    new_password1 = forms.CharField(label=_("New password"), strip=False, widget=forms.PasswordInput(attrs={"autocomplete": "new-password", 'class':'form-input', 'placeholder':'New Password'}), help_text=password_validation.password_validators_help_text_html())
     
-    new_password2 = forms.CharField(label=_("Confirm New password"), strip=False, widget=forms.PasswordInput(attrs={"autocomplete": "new-password", 'class':'form-control'}))
+    new_password2 = forms.CharField(label=_("Confirm New password"), strip=False, widget=forms.PasswordInput(attrs={"autocomplete": "new-password", 'class':'form-input', 'placeholder':'Confirm New Password'}))
 
 
