1e089ae89d7c0a0c5f51f2a2adec6144a084799b
Chiku1022 <charitraagarwal1022@gmail>
2022-04-05 19:52:50 +0530

    updated homepage UI

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,29 +1,30 @@
-let delay = 1000;
-let autoPlay = true;
+function scrollDiv() {
 
-$('#slider1, #slider2, #slider3').owlCarousel({
-    loop: true,
-    margin: 20,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false,
-            autoplay: autoPlay,
-            autoplayTimeout: delay,
-        },
-        600: {
-            items: 3,
-            nav: true,
-            autoplay: autoPlay,
-            autoplayTimeout: delay,
-        },
-        1000: {
-            items: 5,
-            nav: true,
-            loop: true,
-            autoplay: autoPlay,
-            autoplayTimeout: delay,
-        }
+    var productList = document.getElementsByClassName("product-list");
+
+    var checkBoxes = document.getElementsByClassName("autoscroll-checkbox");
+
+    for (let i = 0; i < productList.length; i++) {
+
+      if(checkBoxes[i].checked===false){
+        continue;
+      }
+      const scroll_width = productList[i].scrollWidth;
+      const off_width = productList[i].offsetWidth;
+      const scrollPos = productList[i].scrollLeft;
+      const scrollBy = 10;
+
+      // var debug_msg = `Width:${scroll_width} offset:${off_width} pos:${scrollPos}`;
+      // console.log(debug_msg);
+
+      productList[i].scrollLeft += scrollBy;
+      if (scrollPos === scroll_width - off_width) {
+        productList[i].scrollLeft = 0;
+      }
     }
-})
+  
+}
+
+window.onload = () => {
+  setInterval(scrollDiv, 500);
+};
