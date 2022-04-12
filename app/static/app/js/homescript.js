014cf22d98ba6f8137ab7f17d6dac20e26b3f813
Chiku1022 <charitraagarwal1022@gmail>
2022-04-12 09:44:54 +0530

    renamed js file, removed useless

---
+++ app/static/app/js/homescript.js
@@ -0,0 +1,27 @@
+// Auto scroll list
+function scrollDiv() {
+  var productList = document.getElementsByClassName("product-list");
+  var checkBoxes = document.getElementsByClassName("autoscroll-checkbox");
+
+  for (let i = 0; i < productList.length; i++) {
+    if (checkBoxes[i].checked === false) {
+      continue;
+    }
+    const scroll_width = productList[i].scrollWidth;
+    const off_width = productList[i].offsetWidth;
+    const scrollPos = productList[i].scrollLeft;
+    const scrollBy = 10;
+
+    // var debug_msg = `Width:${scroll_width} offset:${off_width} pos:${scrollPos}`;
+    // console.log(debug_msg);
+
+    productList[i].scrollLeft += scrollBy;
+    if (scrollPos === scroll_width - off_width) {
+      productList[i].scrollLeft = 0;
+    }
+  }
+}
+
+window.onload = () => {
+  setInterval(scrollDiv, 500);
+};
