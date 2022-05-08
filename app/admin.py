53b564073da5145b07c52539b63a9cc177b6ed2d
Chiku1022 <charitraagarwal1022@gmail>
2022-05-08 23:31:56 +0530

    added detailed info column in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 
 from .models import (
     Customer, Product, Cart, OrderPlaced
@@ -19,10 +21,22 @@ class ProductModelAdmin(admin.ModelAdmin):
 
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'product', 'quantity']
+    list_display = ['id', 'user', 'product', 'product_info', 'quantity']
+
+    def product_info(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
 
 
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'customer', 'product', 'quantity',
+    list_display = ['id', 'user', 'customer', 'customer_info', 'product', 'product_info', 'quantity',
                     'ordered_date', 'status', 'txn_id']
+    
+    def customer_info(self, obj):
+        link = reverse("admin:app_customer_change", args=[obj.customer.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.customer.name)
+
+    def product_info(self, obj):
+        link = reverse("admin:app_product_change", args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
