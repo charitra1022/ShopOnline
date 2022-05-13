ae5f342af7c2dc95f53fe9322a306cef74e6334e
Chiku1022 <charitraagarwal1022@gmail>
2022-05-13 19:20:32 +0530

    display only in stock product in homepage

--- app/views.py
+++ app/views.py
@@ -103,7 +103,7 @@ class ProductSneekPeak(View):
         for i in CATEGORY_CHOICES:
             cat_code = i[0]
             cat_name = ''.join(i[1].strip().lower().split())
-            cat_product = Product.objects.filter(category=cat_code)[:5]
+            cat_product = Product.objects.filter(category=cat_code, stock__gt=0)[:5]
             categories[cat_name] = cat_product
         return render(request, 'app/home.html', categories)
 
