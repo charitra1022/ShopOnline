17d6cb1350932367088e723b1379567f704341ee
Charitra Agarwal <charitraagarwal1022@gmail.com>
2024-07-31 01:32:37 +0530

    added allowed host in environment

--- ShopOnline/settings.py
+++ ShopOnline/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
